15fb6348d6410ce5763a31162d25fb7024cab01f
GoldenEagle74 <mshchannikov@yandex.ru>
2022-11-24 14:48:52 +0300

    flake8

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ titles.bloody_valley()
 text.database('start')
 
 game_map.char(1)
-for i in range(2): game_map.char(2)
+for i in range(2):
+    game_map.char(2)
 
 titles.house()
 
@@ -18,14 +19,14 @@ text.database('name')
 name = input()
 system('clear')
 
-text.database('house',name)
+text.database('house', name)
 text.database('choice_1')
 choice = input()
 text.database(choice=choice)
 
 while True:
     location = input('[1]\n[2]\n[3]\n[4]\n[5]\n[6]\n[0] - выход\nВведите локацию: ')
-    if location == '0': break
+    if location == '0':
+        break
     for i in range(2):
-        try: game_map.char(int(location))
-        except: pass
+        game_map.char(int(location))
--- text.py
+++ text.py
@@ -2,9 +2,9 @@ import sys
 import getpass
 from os import system
 from time import sleep
-CURSOR_UP_ONE = '\x1b[1A' 
+CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
-inventory = ['123','321']
+inventory = ['123', '321']
 lastkey = 'start'
 prelastkey = 'start'
 localname = ''
@@ -13,7 +13,8 @@ localname = ''
 def inv():
     global inventory, lastkey, prelastkey, localname
     print('Ваш инвентарь: ')
-    for i in inventory: print(i)
+    for i in inventory:
+        print(i)
     getpass.getpass(prompt='')
     system('clear')
     database(prelastkey, name=localname, f=False)
@@ -25,10 +26,11 @@ def inv():
 
 def database(key='', name='', choice=0, f=True):
     global lastkey, prelastkey, localname
-    if name != '': localname = name
-    a = {0:'name', 1:'house_1', 2:'house_2'}
+    if name != '':
+        localname = name
+    a = {0: 'name', 1: 'house_1', 2: 'house_2'}
     d = {
-        'start':'''В богом забытой деревушке, несущая название “bloody valley” завёлся необычный житель.*
+        'start': '''В богом забытой деревушке, несущая название “bloody valley” завёлся необычный житель.*
 Его волосы цвета угля, его готический стиль, да и эта бледная кожа! Брр!*
 Однако не только внешний вид этого “чертилы” пугал жителей деревни.*
 Спустя какое-то время из деревни начали пропадать молодые люди.*
@@ -36,23 +38,25 @@ def database(key='', name='', choice=0, f=True):
 Кто бы мог подумать что именно он будет искать того самого вора, который каждую ночь спокойно крадет людей из деревни.*
 В которой итак мало людей!*
 ''',
-        'name':'Введите свое имя: ',
-        'house':f'''Вы обнаруживате старого знакомого*
+        'name': 'Введите свое имя: ',
+        'house': f'''Вы обнаруживате старого знакомого*
 {name}: Итааак! Я нашёл себе работу в этой глуши!*
 Фрод: И тебе привет, {name}! Что ты там нашел?*
 {name}: Я нынешний детектив этой глуши!*
 ''',
-    'choice_1':'''Выберите действие:
+        'choice_1': '''Выберите действие:
 [1] Только не мешайся мне под ногами, хорошо???
 [2] И ты мой помощник в этом деле!
 ''',
-    'house_1':'''Фрод: Да как так то...*''',
-    'house_2':'''Фрод: Да как так то...*'''
+        'house_1': '''Фрод: Да как так то...*''',
+        'house_2': '''Фрод: Да как так то...*'''
     }
     flag = True
     for i in ['name', '_']:
-        if i in key or key == '': flag = False
-    if flag: system('clear')
+        if i in key or key == '':
+            flag = False
+    if flag:
+        system('clear')
     if choice == 0:
         for i in d[key]:
             if i != "*":
@@ -66,7 +70,8 @@ def database(key='', name='', choice=0, f=True):
             prelastkey = lastkey
             lastkey = key
     else:
-        if choice == '\x18': inv()
+        if choice == '\x18':
+            inv()
         else:
             for i in d[a[int(choice)]]:
                 if i != "*":
