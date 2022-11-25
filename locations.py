cbe572bc3e6c8b3e71ab0a83e8b791ee5d4db99a
GoldenEagle74 <mshchannikov@yandex.ru>
2022-11-25 11:26:15 +0300

    Добавил фозможность писать сценарий для локаций

--- locations.py
+++ locations.py
@@ -1,4 +1,43 @@
-def square(): print(123)
-def playground(): print(111)
-def dark_corner(): print(333)
-def old_square(): print(321)
+import text
+import sys
+import getpass
+from time import sleep
+from os import system
+nun = False
+
+
+def square():
+    if nun == False:
+        for i in 'Тут нечего делать*':
+                if i != "*":
+                    sleep(0.033)
+                    sys.stdout.write(i)
+                    sys.stdout.flush()
+                else:
+                    getpass.getpass(prompt='')
+        system('clear')
+        text.choose_location()
+def playground():
+    if nun == False:
+        for i in 'Тут нечего делать*':
+                if i != "*":
+                    sleep(0.033)
+                    sys.stdout.write(i)
+                    sys.stdout.flush()
+                else:
+                    getpass.getpass(prompt='')
+        system('clear')
+        text.choose_location()
+def dark_corner():
+    if nun == False:
+        for i in 'Тут нечего делать*':
+                if i != "*":
+                    sleep(0.033)
+                    sys.stdout.write(i)
+                    sys.stdout.flush()
+                else:
+                    getpass.getpass(prompt='')
+        system('clear')
+        text.choose_location()
+def old_square():
+    print('Вы встретили монашку')
