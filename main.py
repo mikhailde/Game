feb828bf1a69637559ae6f9fd13c7764d859532f
GoldenEagle74 <mshchannikov@yandex.ru>
2022-11-28 15:24:01 +0300

    Теперь информация о выборе напарника хранится в main

--- main.py
+++ main.py
@@ -1,41 +1,25 @@
 import game_map
 import titles
 import text
+import locations
 from os import system
 CURSOR_UP_ONE = '\x1b[1A'
 
 
-titles.choose_title('bloody')
-
+titles.bloody_valley()
 text.database('start')
-
 game_map.char(-1)
-for i in range(2):
-    game_map.char(0)
-
-titles.choose_title('house')
-
-text.database('name')
+for i in range(2): game_map.char(0)
+titles.house()
+text.database('name_')
 name = input()
 system('clear')
-
 text.database('house', name)
-text.database('choice_1')
+text.database('choice-1')
 choice = input()
-print(CURSOR_UP_ONE, end="")
+if choice == '1': partner = False
+if choice == '2': partner = True
 text.database(choice=choice)
-
-text.database('note_', inven={'Записка': '''Wo früher das Kloster stand, aber nur eine Nonne blieb
+text.database('note', inven={'Записка': '''Wo früher das Kloster stand, aber nur eine Nonne blieb
          Там где монастырь раньше стоял, но осталась только монашка''', 'Лупа': 'Просто лупа'})
-
-text.choose_location()
-
-
-
-
-while True:
-    location = input('[1]\n[2]\n[3]\n[4]\n[5]\n[6]\n[0] - выход\nВведите локацию: ')
-    if location == '0':
-        break
-    for i in range(2):
-        game_map.char(int(location))
+locations.choose_location(name,partner)
