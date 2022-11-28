ddfd552cdc4b8ff87652ef57050bb93b2b25c65d
GoldenEagle74 <mshchannikov@yandex.ru>
2022-11-28 15:24:38 +0300

    Переписал весь код, убрал все лишнее

--- text.py
+++ text.py
@@ -1,123 +1,74 @@
 import sys
 import getpass
-import game_map
-import titles
 import locations
-from os import system
 from time import sleep
+from os import system
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
 inventory = {}
-lastkey = 'start'
-prelastkey = 'start'
 localname = ''
-a_update = 0
-prechoice = 0
+partner = False
 
 
+def output(text):
+    for i in text:
+        if i != "*":
+            sleep(0.033)
+            sys.stdout.write(i)
+            sys.stdout.flush()
+        else:
+            getpass.getpass(prompt='')
+            print(CURSOR_UP_ONE, end="")
 
-
-def inv(location = False):
-    global inventory, lastkey, prelastkey, localname
+def inv():
+    global inventory, localname, partner
     print('Ваш инвентарь: ')
-    for k, v in inventory.items():
-        print(f'-- {k}: {v}\n')
+    for k, v in inventory.items(): print(f'-- {k}: {v}\n')
     getpass.getpass(prompt='')
-    system('clear')
-    if location: locations.choose_location()
-    else:
-        database(lastkey,f=False)
-        choice = input()
-        print(CURSOR_UP_ONE, end="")
-        database(choice=choice)
-    
+    for i in range(len(inventory)): print(CURSOR_UP_ONE, ERASE_LINE)
+    locations.choose_location(localname, partner)
 
 
-def database(key='', name='', choice=0, f=True, inven = {}, location=False):
-    global lastkey, prelastkey, localname, inventory, a_update
-    if name != '':
-        localname = name
-    if inven:
-        inventory |= inven
-    a = {}
-    if a_update == 0:
-        #a[0] = 'name'
-        a[1] = 'house_1'
-        a[2] = 'house_2'
-        if choice == '1': partner = False
-        if choice == '2': partner = True
-    if a_update == 1:
-        a[1] = locations.square(localname)
-        a[2] = locations.playground(localname)
-        a[3] = locations.dark_corner(localname)
-        a[4] = locations.old_square(localname,partner)
-    d = {
-        'start': '''В богом забытой деревушке, несущая название “bloody valley” завёлся необычный житель.*
+def database(key='', name = '', choice=0, inven = {}):
+    global localname, inventory, partner
+    if name != '': localname = name
+    a = {
+        '1': 'result-1',
+        '2': 'result-2'
+    }
+    text = {
+        'start_': '''В богом забытой деревушке, несущая название “bloody valley” завёлся необычный житель.*
 Его волосы цвета угля, его готический стиль, да и эта бледная кожа! Брр!*
 Однако не только внешний вид этого “чертилы” пугал жителей деревни.*
 Спустя какое-то время из деревни начали пропадать молодые люди.*
 Ох какая же прелесть что новый житель деревни (тот самый “чертила”) оказался детективом.*
 Кто бы мог подумать что именно он будет искать того самого вора, который каждую ночь спокойно крадет людей из деревни.*
-В которой итак мало людей!*
+В которой итак мало людей!*''',
+    'name_': 'Введите свое имя: ',
+    'house': f'''Вы обнаруживате старого знакомого*
+{localname}: Итааак! Я нашёл себе работу в этой глуши!*
+Фрод: И тебе привет, {localname}! Что ты там нашел?*
+{localname}: Я нынешний детектив этой глуши!*
 ''',
-        'name': 'Введите свое имя: ',
-        'house': f'''Вы обнаруживате старого знакомого*
-{name}: Итааак! Я нашёл себе работу в этой глуши!*
-Фрод: И тебе привет, {name}! Что ты там нашел?*
-{name}: Я нынешний детектив этой глуши!*
-''',
-        'choice_1': '''Выберите действие:
+    'choice-1': '''Выберите действие:
 [1] Только не мешайся мне под ногами, хорошо???
 [2] И ты мой помощник в этом деле!
 ''',
-        'house_1': '''Фрод: Да как так то...*''',
-        'house_2': '''Фрод: Да как так то...*''',
-        'note_': '''Фрод: У меня есть записка, которая может тебе помочь в расследовании и лупа, держи*
-Получено: записка, лупа*\n'''
+    'result-1': f'''{CURSOR_UP_ONE}Фрод: Да как так то...*
+''',
+    'result-2': f'''{CURSOR_UP_ONE}Фрод: Да как так то...*
+''',
+    'note': '''Фрод: У меня есть записка, которая может тебе помочь в расследовании и лупа, держи*
+Получено: записка, лупа*'''
     }
-    flag = True
-    for i in ['name', '_']:
-        if i in key or key == '':
-            flag = False
-    if flag:
-        system('clear')
-    if choice == 0:
-        for i in d[key]:
-            if i != "*":
-                sleep(0.033)
-                sys.stdout.write(i)
-                sys.stdout.flush()
-            else:
-                getpass.getpass(prompt='')
-                print(CURSOR_UP_ONE, end="")
-        if f:
-            prelastkey = lastkey
-            lastkey = key
+    if inv: inventory |= inven
+    flag = False
+    if '_' in key: flag = True
+    if flag: system('clear')
+    if choice == 0: output(text[key])
     else:
-        if location:
-            if choice == '\x18':
-                print('true')
-                inv(location=True)
-                return 0
-            if choice == '\x1b':
-                exit()
-            if prechoice != 0:
-                game_map.char(int(prechoice))
-            else: game_map.char(0)
-            for i in range(2): game_map.char(int(choice))
-            titles.choose_title(choice)
-            a[int(choice)]()
-        else:
-            if choice == '\x18':
-                inv()
-                return 0
-            if choice == '\x1b':
-                exit() 
-            for i in d[a[int(choice)]]:
-                if i != "*":
-                    sleep(0.033)
-                    sys.stdout.write(i)
-                    sys.stdout.flush()
-                else:
-                    getpass.getpass(prompt='')
-            a_update += 1
+        if choice == '1': partner = False
+        if choice == '2': partner = True
+        if choice == '\x18': inv()
+        if choice == '\x1b': exit()
+        else: output(text[a[choice]])
